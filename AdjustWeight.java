c9b5bd49a64ad9970a2bca6fbc12f0f516707817
songnezha <593913971@qq.com>
2021-09-25 20:25:17 +0800

    update

--- AdjustWeight.java
+++ AdjustWeight.java
@@ -1,20 +1,28 @@
 import java.io.*;
 import java.util.*;
+import java.io.FileReader;
 
 public class AdjustWeight{
 	private String stage_location;	
 	private String FP_location;
+	private String TP_location;
 	private Map<String, Double> judgeMode2Score = new HashMap<>();
-	private Map<String, Integer> judgeMode2Count = new HashMap<>();
+	private Map<String, Integer> FPJudgeMode2Count = new HashMap<>();
+	private Map<String, Integer> TPJudgeMode2Count = new HashMap<>();
+	private Map<String, Double> FPJudgeMode2Percent = new HashMap<>();
+	private Map<String, Double> TPJudgeMode2Percent = new HashMap<>();
+    private Map<String, Integer> PT2Count = new HashMap<>();
 	private ArrayList<String> FPs = new ArrayList<String>();
+    private ArrayList<String> TPs = new ArrayList<String>();
 	private double threshold;
 	
-	public AdjustWeight(String stage_location, String FP_location) {
+	public AdjustWeight(String stage_location, String FP_location, String TP_location) {
 		this.stage_location = stage_location;
 		this.FP_location = FP_location;
+		this.TP_location = TP_location;
 	}
 	
-	private void prepareInfo() {	
+	public void prepareInfo() {
 		try {
 			String line;
 			BufferedReader stageReader = new BufferedReader(new FileReader(stage_location));
@@ -26,29 +34,70 @@ public class AdjustWeight{
 			}
 		}catch (IOException e) { 
             e.printStackTrace(); 
-        } 
-		
-		File FPdir = new File(FP_location);
+        }
+
+        func(FP_location, FPs, FPJudgeMode2Count);
+		func(TP_location, TPs, TPJudgeMode2Count);
+
+		int sumFP = 0, sumTP = 0;
+
+		for(Map.Entry<String, Integer> entry : FPJudgeMode2Count.entrySet()){
+			System.out.println("FP: " + entry.getKey() + " " + entry.getValue());
+			sumFP += entry.getValue();
+		}
+
+		for(Map.Entry<String, Integer> entry : TPJudgeMode2Count.entrySet()){
+			System.out.println("TP: " + entry.getKey() + " " + entry.getValue());
+			sumTP += entry.getValue();
+		}
+
+		for(Map.Entry<String, Integer> entry : FPJudgeMode2Count.entrySet()){
+			double percent = (double)entry.getValue() / sumFP;
+			FPJudgeMode2Percent.put(entry.getKey(), percent);
+			System.out.println("FP: " + entry.getKey() + " " + percent);
+		}
+
+		for(Map.Entry<String, Integer> entry : TPJudgeMode2Count.entrySet()){
+			double percent = (double)entry.getValue() / sumTP;
+			TPJudgeMode2Percent.put(entry.getKey(), percent);
+			System.out.println("TP: " + entry.getKey() + " " + percent);
+		}
+
+	}
+
+	private void func(String location, ArrayList<String> FPs, Map<String, Integer> judgeMode2Count) {
+		System.out.println(location);
+        File FPdir = new File(location);
         File[] FPfiles = FPdir.listFiles();
         for (int i = 0; i < FPfiles.length; i++) {
-        	FPs.add(FPfiles[i].getName());
+            FPs.add(FPfiles[i].getName());
         }
-		
-		for(int i = 0; i < FPs.size(); i++) {
-			String[] slices = FPs.get(i).substring(0, FPs.get(i).length()-4).split("_");
-			for(int j = 0; j < slices.length; j++) {
-				if(slices[j].equals("result")) {
-					continue;
-				}
-				if(judgeMode2Count.containsKey(slices[j])) {
-					judgeMode2Count.put(slices[j], judgeMode2Count.get(slices[j])+1);
-				}
-				else {
-					judgeMode2Count.put(slices[j], 1);
-				}
+
+        for(int i = 0; i < FPs.size(); i++) {
+        	int lines = 0;
+        	try {
+				FileReader in = new FileReader(FPfiles[i]);
+				LineNumberReader reader = new LineNumberReader(in);
+				reader.skip(Long.MAX_VALUE);
+				lines = reader.getLineNumber();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		}
-	}
+            String[] slices = FPs.get(i).substring(0, FPs.get(i).length()-4).split("_");
+            for(int j = 0; j < slices.length; j++) {
+                if(slices[j].equals("result")) {
+                    continue;
+                }
+                if(judgeMode2Count.containsKey(slices[j])) {
+                    judgeMode2Count.put(slices[j], judgeMode2Count.get(slices[j]) + lines);
+                }
+                else {
+                    judgeMode2Count.put(slices[j], lines);
+                }
+            }
+        }
+        System.out.println(judgeMode2Count.size());
+    }
 	
 	private void writeTofile() {
 		// remain to be code
@@ -58,48 +107,59 @@ public class AdjustWeight{
 		}
 		System.out.println("\nthreshold:\n"+threshold);
 	}
-	
+
 	public void adjust() {
 		prepareInfo();
 		
-		// misunderstanding. Another policy?
-		/*
-		Double v;
-		for (String key : judgeMode2Count.keySet()) {
-			v = Double.valueOf(judgeMode2Count.get(key))/Double.valueOf(FPs.size());
-			judgeMode2Score.put(key, judgeMode2Score.get(key)-v);
+		for(Map.Entry<String, Double> m : judgeMode2Score.entrySet()) {
+			String point = m.getKey();
+			double score = m.getValue();
+			double newScore = score +  (TPJudgeMode2Percent.getOrDefault(point, 0.0) -
+					FPJudgeMode2Percent.getOrDefault(point, 0.0)) * score * 2.5;
+			judgeMode2Score.put(point, newScore);
 		}
-		*/
-		
-		Double v;
-		for (String key : judgeMode2Count.keySet()) {
-			v = Double.valueOf(FPs.size())/Double.valueOf(judgeMode2Count.get(key));
-			judgeMode2Score.put(key, v);
+
+		for(Map.Entry<String, Double> m : judgeMode2Score.entrySet()) {
+			if(TPJudgeMode2Percent.containsKey(m.getKey()) || FPJudgeMode2Percent.containsKey(m.getKey())) {
+				System.out.println(m.getKey() + ": " + m.getValue());
+			}
 		}
-		
-		threshold = -1.0;
-		double tScore = 0;
+
 		for(int i = 0; i < FPs.size(); i++) {
+			double sum = 0;
 			String[] slices = FPs.get(i).substring(0, FPs.get(i).length()-4).split("_");
 			for(int j = 0; j < slices.length; j++) {
 				if(slices[j].equals("result")) {
 					continue;
 				}
-				tScore = tScore + judgeMode2Score.get(slices[j]);
+				sum += judgeMode2Score.get(slices[j]);
 			}
-			if(tScore > threshold) {
-				threshold = tScore;
-			}
-			tScore = 0;
+			System.out.println("Sum of FP File" + i + " is: " + sum);
 		}
-		
-		
-		writeTofile();
+
+		for(int i = 0; i < TPs.size(); i++) {
+			double sum = 0;
+			String[] slices = TPs.get(i).substring(0, TPs.get(i).length()-4).split("_");
+			for(int j = 0; j < slices.length; j++) {
+				if(slices[j].equals("result")) {
+					continue;
+				}
+				sum += judgeMode2Score.get(slices[j]);
+			}
+			System.out.println("Sum of TP File" + i + " is: " + sum);
+		}
+
+
+
+		//writeTofile();
 	}
-	
+
 	public static void main(String[] args) {
 	    // for test
-		AdjustWeight adjustWeight = new AdjustWeight("/home/lesion/Desktop/detector/stage.txt","/home/lesion/detector/txt/123");
+		AdjustWeight adjustWeight = new AdjustWeight(
+		        "C:\\Users\\59391\\Desktop\\adjustWeight\\stage.txt",
+                "C:\\Users\\59391\\Desktop\\adjustWeight\\123",
+                "C:\\Users\\59391\\Desktop\\adjustWeight\\234");
 		adjustWeight.adjust();
 	}
 }
