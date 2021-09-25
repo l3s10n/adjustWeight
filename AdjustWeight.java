70a2096b80358e62fb14a7c1b5afdd07553cdbda
lesion1999 <72327909+lesion1999@users.noreply.github.com>
2021-09-25 15:07:26 +0800

    Update AdjustWeight.java

--- AdjustWeight.java
+++ AdjustWeight.java
@@ -7,13 +7,14 @@ public class AdjustWeight{
 	private Map<String, Double> judgeMode2Score = new HashMap<>();
 	private Map<String, Integer> judgeMode2Count = new HashMap<>();
 	private ArrayList<String> FPs = new ArrayList<String>();
+	private double threshold;
 	
 	public AdjustWeight(String stage_location, String FP_location) {
 		this.stage_location = stage_location;
 		this.FP_location = FP_location;
 	}
 	
-	private void initInfo() {	
+	private void prepareInfo() {	
 		try {
 			String line;
 			BufferedReader stageReader = new BufferedReader(new FileReader(stage_location));
@@ -27,38 +28,78 @@ public class AdjustWeight{
             e.printStackTrace(); 
         } 
 		
-		try {
-			File FPdir = new File(FP_location);
-	        File[] FPfiles = FPdir.listFiles();
-	        for (int i = 0; i < FPfiles.length; i++) {
-	        	FPs.add(FPfiles[i].getName());
-	        }
-		}catch (IOException e) { 
-            e.printStackTrace(); 
+		File FPdir = new File(FP_location);
+        File[] FPfiles = FPdir.listFiles();
+        for (int i = 0; i < FPfiles.length; i++) {
+        	FPs.add(FPfiles[i].getName());
         }
 		
 		for(int i = 0; i < FPs.size(); i++) {
-			String[] slices = FPs[i].split("_");
+			String[] slices = FPs.get(i).substring(0, FPs.get(i).length()-4).split("_");
 			for(int j = 0; j < slices.length; j++) {
-				if(slices[i] == "result") {
-					countinue;
+				if(slices[j].equals("result")) {
+					continue;
 				}
-				if(judgeMode2Count.containsKey(slices[i])) {
-					judgeMode2Count.put(slices[i], judgeMode2Count.get(slices[i])+1);
+				if(judgeMode2Count.containsKey(slices[j])) {
+					judgeMode2Count.put(slices[j], judgeMode2Count.get(slices[j])+1);
 				}
 				else {
-					judgeMode2Count.put(slices[i], 1);
+					judgeMode2Count.put(slices[j], 1);
 				}
 			}
 		}
 	}
 	
-	public void adjust() {
-		initInfo();
-		
+	private void writeTofile() {
+		// remain to be code
+		for (String key : judgeMode2Score.keySet()) {
+			System.out.println(key);
+			System.out.println(judgeMode2Score.get(key));
+		}
+		System.out.println("\nthreshold:\n"+threshold);
 	}
-}
-
-public static void main(String[] args) {
-    System.out.print(1);
+	
+	public void adjust() {
+		prepareInfo();
+		
+		// misunderstanding. Another policy?
+		/*
+		Double v;
+		for (String key : judgeMode2Count.keySet()) {
+			v = Double.valueOf(judgeMode2Count.get(key))/Double.valueOf(FPs.size());
+			judgeMode2Score.put(key, judgeMode2Score.get(key)-v);
+		}
+		*/
+		
+		Double v;
+		for (String key : judgeMode2Count.keySet()) {
+			v = Double.valueOf(FPs.size())/Double.valueOf(judgeMode2Count.get(key));
+			judgeMode2Score.put(key, v);
+		}
+		
+		threshold = -1.0;
+		double tScore = 0;
+		for(int i = 0; i < FPs.size(); i++) {
+			String[] slices = FPs.get(i).substring(0, FPs.get(i).length()-4).split("_");
+			for(int j = 0; j < slices.length; j++) {
+				if(slices[j].equals("result")) {
+					continue;
+				}
+				tScore = tScore + judgeMode2Score.get(slices[j]);
+			}
+			if(tScore > threshold) {
+				threshold = tScore;
+			}
+			tScore = 0;
+		}
+		
+		
+		writeTofile();
+	}
+	
+	public static void main(String[] args) {
+	    // for test
+		AdjustWeight adjustWeight = new AdjustWeight("/home/lesion/Desktop/detector/stage.txt","/home/lesion/detector/txt/123");
+		adjustWeight.adjust();
+	}
 }
